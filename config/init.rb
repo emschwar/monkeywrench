635217c4d0f4440af69a58b8915515e824d13c09
Eric Schwartz <eric.schwartz@centro.net>
2009-10-21 08:17:40 -0500

    merb-gen output

---
+++ config/init.rb
@@ -0,0 +1,24 @@
+# Go to http://wiki.merbivore.com/pages/init-rb
+ 
+require 'config/dependencies.rb'
+ 
+use_orm :datamapper
+use_test :rspec
+use_template_engine :erb
+ 
+Merb::Config.use do |c|
+  c[:use_mutex] = false
+  c[:session_store] = 'cookie'  # can also be 'memory', 'memcache', 'container', 'datamapper
+  
+  # cookie session store configuration
+  c[:session_secret_key]  = 'a03c3f7b0f87fe1dcd56665bcb81320a823ab109'  # required for cookie session store
+  c[:session_id_key] = '_monkeywrench_session_id' # cookie session id key, defaults to "_session_id"
+end
+ 
+Merb::BootLoader.before_app_loads do
+  # This will get executed after dependencies have been loaded but before your app's classes have loaded.
+end
+ 
+Merb::BootLoader.after_app_loads do
+  # This will get executed after your app's classes have been loaded.
+end
